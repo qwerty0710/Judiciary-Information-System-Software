80b8dab3a8cf8dfd068125c9df342cce8a36b36f
nespar7 <nsuryaprakashreddy1234@gmail.com>
2022-06-24 20:30:42 +0530

    added user model

---
+++ backend/model/Users.model.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    designation: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// exporting the user schema as User
+module.exports = mongoose.model('User', User);
