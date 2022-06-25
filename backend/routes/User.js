ee29fa3671474be42cbf42acfd4508b1e14955f1
nespar7 <nsuryaprakashreddy1234@gmail.com>
2022-06-25 10:18:27 +0530

    added user router

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -6,9 +6,41 @@ var User = require('../model/Users.model');
 router.route('/').get((req, res) => {
     User.find()
         .then(users => res.status(200).json(users))
-        .catch(err => res.status(500).send('Error ' + err));
+        .catch(err => res.status(500).send({error: err}));
 });
 
 router.route('/add').post((req, res) => {
-    const username = req.body
+
+    var user = new User();
+
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.designation = req.body.designation;
+
+    user.save((err, savedUser) => {
+        if (err) {
+            res.status(500).send({ error: err });
+        }
+        else {
+            res.send(savedUser);
+        }
+    });
+});
+
+router.route('/login').post((req, res) => {
+    User.findOne({ username: req.body.username, password: req.body.password })
+        .then(doc => {
+            console.log(doc);
+            if (doc) {
+                res.json({ username: doc.username, success: true });
+            }
+            else {
+                res.json({ success: false });
+            }
+        })
+        .catch(err => {
+            res.status(500).json('failed');
+        })
 })
+
+module.exports = router;
