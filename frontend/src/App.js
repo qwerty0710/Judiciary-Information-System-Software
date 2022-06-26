70468cd3ec66c35bfb5e34a9e2b96134ed7b971e
nespar7 <nsuryaprakashreddy1234@gmail.com>
2022-06-26 22:14:10 +0530

    Added all the Routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Login from './Components/Login';
+import CaseList from './Components/CaseList';
+import CreateUser from './Components/CreateUser';
+import CreateCase from './Components/CreateCase';
+import UpdateCase from './Components/UpdateCase';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className='navbar-container'>
+				<Navbar />
+				<br />
+				<Route path='/' exact element={Login} />
+				<Route path='/caseList' exact element={CaseList} />
+				<Route path='/update/:id' element={UpdateCase} />
+				<Route path='/create' element={CreateCase} />
+				<Route path='/RegisterUser' element={CreateUser} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
